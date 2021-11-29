8882feadb5b678d35a26acbf0049fe84dcdc3036
SacredDevKing <sacreddevking@gmail.com>
2021-11-29 00:33:12 -0300

    end

--- resources/js/Pages/PrivacyPolicy.vue
+++ resources/js/Pages/PrivacyPolicy.vue
@@ -9,6 +9,7 @@
                 </div>
 
                 <div v-html="policy" class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
+                    
                 </div>
             </div>
         </div>
